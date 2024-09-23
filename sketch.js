bdcb5e774e983d37ba9383e62fd20d6b4960e66c
xBellzys <bellinhazinha10@gmail.com>
2024-09-23 14:25:55 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  Function draw() {
+    stroke("blue");
+    fill("red");
+    
+    1f(mouseIsPressed){
+       rect(mouseX, mouseY, 20, 35);
+    
+    }
+  
+  }
